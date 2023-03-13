96f107626b078452b2bff3ea170b092bcdbe62d7
Stephenoco <s.oconnor73@nuigalway.ie>
2023-03-13 12:40:10 +0000

    Update Settings.vue
    Settings Page now submits User's data

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -14,14 +14,11 @@ export default {
     return {}
   },
   methods: {
-    swapContent(div_id){
-      const main = document.getElementById('main');
-      const div = document.getElementById(div_id);
-      const clone = div.cloneNode(true);
-
-      while (main.firstChild) main.firstChild.remove();
-
-      main.appendChild(clone);
+    swapContent(div1,div2){
+      let divA = document.getElementById(div1);
+      let divB = document.getElementById(div2);
+        divA.style.display = 'block';
+        divB.style.display = 'none';
     },
     getUserData() {
       const dataRequest = httpsCallable(functions, 'GetUserData');
@@ -29,22 +26,20 @@ export default {
         const userdata = result.data;
         console.log(userdata);
         document.getElementById('firstName').placeholder = userdata['firstName'];
-        document.getElementById('surname').placeholder = userdata['surName'];
+        document.getElementById('surName').placeholder = userdata['surName'];
         document.getElementById('username').placeholder = userdata['username'];
         document.getElementById('dob').placeholder = userdata['dob'];
         document.getElementById('email').placeholder = userdata['email'];
         document.getElementById('bio').placeholder = userdata['bio'];
         document.getElementById('course').placeholder = userdata['courseCode'];
-        document.getElementById('newPassword').placeholder = userdata['newPassword'];
-        document.getElementById('passwordConfirm').placeholder = userdata['passwordConfirm'];
 
       });
 
 
     },
     submitUserData() {
-      let userData = [];
-        let password = document.getElementById('newPassword');
+      let userData = {};
+      let password = document.getElementById('newPassword');
         let passwordConfirm = document.getElementById('passwordConfirm');
         let firstName = document.getElementById('firstName');
         let surName = document.getElementById('surName');
@@ -54,7 +49,7 @@ export default {
 
         if (password.value !== "" && passwordConfirm.value !== "") {
           if (password.value === passwordConfirm.value) {
-            userData['password'] = document.getElementById("newPassword");
+            userData['password'] = document.getElementById("newPassword").value;
             password.classList.remove("is-invalid")
             password.classList.add("is-valid")
           }
@@ -63,55 +58,21 @@ export default {
             password.classList.add("is-invalid")
           }
         }
-        else {
-          password.classList.remove("is-valid")
-          password.classList.add("is-invalid")
-        }
         if (firstName.value !== '') {
-          userData['firstName'] = document.getElementById("firstName");
-          firstName.classList.remove("is-invalid")
-          firstName.classList.add("is-valid")
-        }
-        else {
-          firstName.classList.remove("is-valid")
-          firstName.classList.add("is-invalid")
+          userData['firstName'] = document.getElementById("firstName").value;
         }
         if (surName.value !== '') {
-          userData['surName'] = document.getElementById("surname");
-          surName.classList.remove("is-invalid")
-          surName.classList.add("is-valid")
-        }
-        else {
-          surName.classList.remove("is-valid")
-          surName.classList.add("is-invalid")
+          userData['surName'] = document.getElementById("surName").value;
         }
         if (username.value !== '') {
-          userData['username'] = document.getElementById("username");
-          username.classList.remove("is-invalid")
-          username.classList.add("is-valid")
-        }
-        else {
-          username.classList.remove("is-valid")
-          username.classList.add("is-invalid")
+          userData['username'] = document.getElementById("username").value;
         }
         if (dob.value !== '') {
-          userData['dob'] = document.getElementById("dob");
-          dob.classList.remove("is-invalid")
-          dob.classList.add("is-valid")
-        }
-        else {
-          dob.classList.remove("is-valid")
-          dob.classList.add("is-invalid")
+          userData['dob'] = document.getElementById("dob").value;
         }
 
         if (email.value !== ''){
-          userData['email'] = document.getElementById('email');
-          email.classList.remove("is-invalid")
-          email.classList.add("is-valid")
-        }
-        else {
-          email.classList.remove("is-valid")
-          email.classList.add("is-invalid")
+          userData['email'] = document.getElementById('email').value;
         }
         console.log(userData);
         const dataUpload = httpsCallable(functions, 'SubmitUserData');
@@ -121,23 +82,15 @@ export default {
     },
     submitProfileDetails() {
       let userData = {};
-        let bio = document.getElementById('bio');
-        let course = document.getElementById('course');
+      let bio = document.getElementById('bio');
+      let course = document.getElementById('course');
         if (bio.value !== '') {
           userData['bio'] = document.getElementById("bio").value;
-          bio.classList.remove("is-invalid")
-          bio.classList.add("is-valid")
         } else {
-          bio.classList.remove("is-valid")
-          bio.classList.add("is-invalid")
         }
         if (course.value !== '') {
           userData['courseCode'] = document.getElementById("course").value;
-          course.classList.remove("is-invalid")
-          course.classList.add("is-valid")
         } else {
-          course.classList.remove("is-valid")
-          course.classList.add("is-invalid")
         }
       console.log(userData);
         const dataUpload = httpsCallable(functions, 'SubmitUserData');
@@ -160,23 +113,62 @@ export default {
           <div>
           <ul class="nav flex-column nav-pills">
             <li class="nav-item pills">
-              <button @click="getUserData()" class="nav-link " id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="false">Settings</button>
+              <button @click="swapContent('changeDetails','changeProfile'),getUserData()" class="nav-link " id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="false">Settings</button>
             </li>
             <li class="nav-item pills">
-              <button @click="getUserData()" class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
-            </li>
-            <li class="nav-item pills">
-              <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Something else</button>
-            </li>
-            <li class="nav-item pills">
-              <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Account Details</button>
+              <button @click="swapContent('changeProfile','changeDetails'),getUserData()" class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
             </li>
           </ul>
           </div>
       </div>
-      <div class="col-md-10 main " id="main">
-        <div>
-          <div id="changeProfile">
+      <div class="col-md-10 main" id="changeDetails" style="display: none">
+            <form class="needs-validation" novalidate>
+              <div class="input">
+                <div class="col">
+                  <h4>Change User Details</h4>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label for="firstName" class="form-label">First Name</label>
+                    <input id="firstName" class="form-control" placeholder="First name">
+                  </div>
+                  <div class="col">
+                    <label for="surName" class="form-label">Surname</label>
+                    <input id="surName" class="form-control" placeholder="surName">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <label class="form-label">Username</label>
+                    <input id="username" class="form-control" placeholder="Username">
+                  </div>
+                  <div class="col">
+                    <label for="email" class="form-label">Email</label>
+                    <input id="email" class="form-control" placeholder="Email">
+                  </div>
+                </div>
+                <div>
+                  <label for="dob" class="form-label">Date of Birth</label>
+                  <input id="dob" class="form-control" placeholder="dd/mm/yyyy">
+                </div>
+                <div>
+                  <label class="form-label">New Password</label>
+                  <input id="newPassword" class="form-control">
+                </div>
+                <div>
+                  <label class="form-label">Confirm New Password</label>
+                  <input id="passwordConfirm" class="form-control">
+                </div>
+                <div>
+                  <br>
+                  <button type="button" class="btn btn-secondary" @click="submitUserData">Save</button>
+                </div>
+              </div>
+            </form>
+        </div>
+
+
+        <div class="col-md-10 main" style="display: none" id="changeProfile" >
             <form class="needs-validation" novalidate>
               <div class="input">
                 <div class="col">
@@ -205,10 +197,10 @@ export default {
                         <label for="bio" class="form-label">Bio</label>
                         <textarea id="bio" class="form-control" placeholder="bio"></textarea>
                       </div>
-                      <div>
-                        <br>
-                        <button type="button" class="btn btn-secondary" @click="submitProfileDetails">Save</button>
-                      </div>
+                    </div>
+                    <div>
+                      <br>
+                      <button type="button" class="btn btn-secondary" @click="submitProfileDetails">Save</button>
                     </div>
                   </div>
                 </div>
@@ -216,56 +208,7 @@ export default {
             </form>
           </div>
         </div>
-      </div>
-  <div style="display: none">
-        <div id="changeDetails">
-          <form class="needs-validation" novalidate>
-            <div class="input">
-              <div class="col">
-                <h4>Change User Details</h4>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <label for="firstName" class="form-label">First Name</label>
-                  <input id="firstName" class="form-control" placeholder="First name" required>
-                </div>
-                <div class="col">
-                  <label for="surname" class="form-label">Surname</label>
-                  <input id="surname" class="form-control" placeholder="Surname">
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="col">
-                  <label class="form-label">Username</label>
-                  <input id="username" class="form-control" placeholder="Username">
-                </div>
-                <div class="col">
-                  <label for="email" class="form-label">Email</label>
-                  <input id="email" class="form-control" placeholder="Email">
-                </div>
-              </div>
-              <div>
-                <label for="dob" class="form-label">Date of Birth</label>
-                <input id="dob" class="form-control" placeholder="dd/mm/yyyy">
-              </div>
-              <div >
-                <label class="form-label">New Password</label>
-                <input id="newPassword" class="form-control">
-              </div>
-              <div >
-                <label class="form-label">Confirm New Password</label>
-                <input id="passwordConfirm" class="form-control">
-              </div>
-              <div>
-                <button type="button" class="btn btn-secondary" @click="submitUserData">Save</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
